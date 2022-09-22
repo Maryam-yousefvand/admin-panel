5ed810e91528a906f1cb13f8958a6cf625617a62
Maryam-yousefvand <79748261+Maryam-yousefvand@users.noreply.github.com>
2022-09-22 21:53:15 +0330

    Update api.jsx

--- panel/src/features/api.jsx
+++ panel/src/features/api.jsx
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
 //   baseURL: 'http://localhost:4000',
-  baseURL: 'https://admin-panel01.herokuapp.com/',
+  baseURL: 'https://admin-panel77.herokuapp.com/',
 })
